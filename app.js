0a303e1010b92e8668cbe2755e8a67030bfddc3b
Fey <Feyliec0@gmail.com>
2026-08-10 15:42:30 -0400

    Review fix

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "6a6e5a311e31d80889689ae1",
+  };
+
+  next();
+});
+
 app.use("/", router);
 
 app.use((req, res) => {
